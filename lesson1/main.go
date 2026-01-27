5a9b8d3051d231156fca462b52048407b4bcb4f7
Liberbell <liberbell@gmail.com>
2026-01-28 08:32:52 +0900

    main5

--- lesson1/main.go
+++ lesson1/main.go
@@ -6,7 +6,13 @@ import (
 	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte{"Running API v1...\n"})
+}
+
 func main() {
+	http.HandleFunc(":", rootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
