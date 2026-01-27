43c1014053e064c085260a3f2b8d48c961542224
Liberbell <liberbell@gmail.com>
2026-01-28 08:54:11 +0900

    main9

--- lesson1/main.go
+++ lesson1/main.go
@@ -10,6 +10,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("Asset not found\n"))
+		return
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Running API v1...\n"))
