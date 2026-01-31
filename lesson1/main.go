68b4583c307cb0c869de49da73c42ec7124bc9b1
Liberbell <liberbell@gmail.com>
2026-01-31 10:43:50 +0900

    main1

--- lesson1/main.go
+++ lesson1/main.go
@@ -6,16 +6,6 @@ import (
 	"os"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Asset not found\n"))
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Running API v1...\n"))
-}
-
 func main() {
 	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
