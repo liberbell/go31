67f625078c2a34f1a89128fd403a17fd62cadbdd
Liberbell <liberbell@gmail.com>
2026-02-01 12:18:09 +0900

    main4

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"Lesson1/rest_handlers"
 	"fmt"
-	"lesson1/rest_handlers"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", rest_handlers.RootHandler)
-	http.HandleFunc("/users", rest_handlers.UserHandler)
+	http.HandleFunc("/users", rest_handlers.UsersRouter)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
