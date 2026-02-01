f3d5c2dad9320e5a39a6c8d5af39a7a39c4a76b7
Liberbell <liberbell@gmail.com>
2026-02-02 08:40:15 +0900

    responses3

--- lesson1/rest_handlers/responses.go
+++ lesson1/rest_handlers/responses.go
@@ -2,6 +2,6 @@ package handlers
 
 import "net/http"
 
-func postError(w http.ResponseWriter, code int) {
+func PostError(w http.ResponseWriter, code int) {
 	http.Error(w, http.StatusText(code), code)
 }
