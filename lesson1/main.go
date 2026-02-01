2e8cb1f14ef13e1413073c0c529caed764e860a4
Liberbell <liberbell@gmail.com>
2026-02-02 08:18:19 +0900

    main1

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"Lesson1/rest_handlers"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+func usersRouter(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Asset not found\n"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1...\n"))
+}
+
 func main() {
-	http.HandleFunc("/", rest_handlers.RootHandler)
-	http.HandleFunc("/users", rest_handlers.UsersRouter)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/users", usersRouter)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
