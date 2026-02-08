76277b73d182bbe29a0d61d047c647f30427483b
Liberbell <liberbell@gmail.com>
2026-02-08 16:28:03 +0900

    main6

--- lesson1/main.go
+++ lesson1/main.go
@@ -67,7 +67,7 @@ func usersRouter(w http.ResponseWriter, r *http.Request) {
 		usersGetOne(w, r, id)
 		return
 	case http.MethodOptions:
-		postOptionsResponse(w, []string{http.MethodGet, http.MethodPut, http.MethodDelete, http.MethodPatch http.MethodHead, http.MethodOptions}, nil)
+		postOptionsResponse(w, []string{http.MethodGet, http.MethodPut, http.MethodDelete, http.MethodPatch, http.MethodHead, http.MethodOptions}, nil)
 		return
 	default:
 		postError(w, http.StatusMethodNotAllowed)
