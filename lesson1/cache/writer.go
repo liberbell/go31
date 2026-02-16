942414f9eb532ec181ec400950117de0e1e5fd99
Liberbell <liberbell@gmail.com>
2026-02-17 08:27:04 +0900

    writer3

--- lesson1/cache/writer.go
+++ lesson1/cache/writer.go
@@ -11,3 +11,13 @@ type Writer struct {
 var (
 	_ http.ResponseWriter = (*Writer)(nil)
 )
+
+func NewWriter(w http.ResponseWriter, r *http.Request) *Writer {
+	return &Writer{
+		writer:   w,
+		resource: MakeResource(r),
+		response: response{
+			header: http.Header{},
+		},
+	}
+}
