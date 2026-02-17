429c5c357c001e6031bf7419f00cc4ad4484507d
Liberbell <liberbell@gmail.com>
2026-02-18 08:28:31 +0900

    writer_test3

--- lesson1/cache/writer_test.go
+++ lesson1/cache/writer_test.go
@@ -10,3 +10,13 @@ func newMockWriter() *mockWriter {
 		header: http.Header{},
 	}
 }
+
+func (mw *mockWriter) Write(b []byte) (int, error) {
+	mw.body = make([]byte, len(b))
+	for k, v := range b {
+		w.response.body[k] = v
+	}
+	copyHeader(w.Header(), w.writer.Header())
+	set(w.resource, &w.response)
+	return w.writer.Write(b)
+}
