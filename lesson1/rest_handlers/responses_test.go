3717451167ce90a37adeec354edc6b6b02656254
Liberbell <liberbell@gmail.com>
2026-02-20 07:11:12 +0900

    responses_test2

--- lesson1/rest_handlers/responses_test.go
+++ lesson1/rest_handlers/responses_test.go
@@ -1,10 +1,20 @@
 package handlers
 
+import "net/http"
+
 type mockWriter response
 
 func newMockWriter() *mockWriter {
 	return &mockWriter{
-		body:   []body{},
-		header: http.header{},
+		body:   []byte{},
+		header: http.Header{},
 	}
+}
+
+func (mw *mockWriter) Write(b []byte) (int, error) {
+	mw.body = make([]byte, len(b))
+	for k, v := range b {
+		mw.body[k] = v
+	}
+	return len(b), nil
 }
