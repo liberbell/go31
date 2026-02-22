d2dc0322b2a8da68cce990c03fbbed388a469edb
Liberbell <liberbell@gmail.com>
2026-02-22 15:33:06 +0900

    main2

--- lesson1/echo/main.go
+++ lesson1/echo/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func root(c echo.Context) error {
+	return c.String(http.StatusOK, "Running API v1")
+}
 
 func main() {
 	e := echo.New()
+
+	e.GET("/", root)
+
 	e.Start(":12345")
 }
