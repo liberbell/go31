a3d6b80c1cc56ee5b5820ef25fc68e42955a5aa2
Liberbell <liberbell@gmail.com>
2026-02-23 14:14:17 +0900

    main3

--- lesson1/echo/main.go
+++ lesson1/echo/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"net/http"
+	"restapi/cache"
+	"restapi/user"
 	"strings"
 
+	"github.com/asdine/storm"
 	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func usersOptions(c echo.Context) error {
@@ -17,6 +21,52 @@ func usersOptions(c echo.Context) error {
 	methods := []string{http.MethodGet, http.MethodPost, http.MethodPatch, http.MethodDelete, http.MethodHead, http.MethodOptions}
 	c.Response().Header().Set("Allow", strings.Join(methods, ","))
 	c.NoContent(http.StatusOK)
+}
+
+func usersPatchOne(w http.ResponseWriter, r *http.Request, id bson.ObjectId) {
+	u, err := user.One(id)
+	if err != nil {
+		if err == storm.ErrNotFound {
+			postError(w, http.StatusNotFound)
+			return
+		}
+		postError(w, http.StatusInternalServerError)
+		return
+	}
+	err = bodyToUser(r, u)
+	if err != nil {
+		postError(w, http.StatusBadRequest)
+		return
+	}
+
+	u.ID = id
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			postError(w, http.StatusBadRequest)
+		} else {
+			postError(w, http.StatusInternalServerError)
+		}
+		return
+	}
+	cache.Drop("/users")
+	cw := cache.NewWriter(w, r)
+	postBodyResponse(cw, http.StatusOK, jsonResponse{"user": u})
+}
+
+func usersDeleteOne(c echo.Context, id bson.ObjectId) {
+	err := user.Delete(id)
+	if err != nil {
+		if err == storm.ErrNotFound {
+			postError(w, http.StatusNotFound)
+			return
+		}
+		postError(w, http.StatusInternalServerError)
+		return
+	}
+	cache.Drop("/users")
+	cache.Drop(cache.MakeResource(r))
+	w.WriteHeader(http.StatusOK)
 }
 
 func root(c echo.Context) error {
